3e4950119f39e1a15d9c806392e39942d9a95c05
Vladimir <I3rovvser@gmail.com>
2020-12-07 14:01:49 +0300

    We've started with included event loop
    Do i need to use flow-model for version-control, when i haven't any version?

--- src/client.py
+++ src/client.py
@@ -4,61 +4,91 @@ import websockets
 uri = 'ws://irc-ws.chat.twitch.tv:80'
 
 def split(text: str, separator: str, max = 0):
+    # get len of separator
     sep_len = len(separator)
+    # if len is 0, it must be Exception
+    # but better we just return whole string
     if sep_len == 0:
-        return text
+        yield text
+        raise StopIteration
+    # counter counts times we yielded string
     counter = 0
+    # i - just index of letter in str
     i = 0
+    # simple condition, for check whole string
+    # we need (i+sep_len) in condition
+    # for don't get IndexError - Exception 
+    #
+    # example: if len of text = 5, len of sep = 2,
+    # then the last index we'll can check is 4,
+    # and in the 28th row we'll get text[4:6],
+    # but we haven't 5th index of text, so we'll get Exception
     while i+sep_len < len(text):
+        #look for separator in text 
         if text[i:i+sep_len] == separator:
+            # after find, yeild all before separator
             yield text[:i]
             counter += 1
+            # if counts of times we yielded parts equals max(from args)
+            # we need to stop this generator
             if counter == max:
-                return None
+                raise StopIteration
+            # if we continue we need to delete previous part and separator from text
             text = text[i+sep_len:]
+            # we've deleted previous part, 
+            # so we need to continue searching from 0th index 
             i = 0
+        # just increase the index
         i += 1
+    # if out condition (i+sep_len < len(text)) is not True
+    # we'll get here
     else:
+        # if text endswith the separator we need th delete it
         if text[-sep_len:] == separator:
             yield text[:-sep_len]
+        # else we don't need to delete
         else:
             yield text
+    # without comments it look simpler
 
-class Client:
+class Client: 
 
     def __init__(self, token, nick, channels):
         self.token = token
         self.nick = nick
         self.channels = channels
+        self.ws = None
+        self.event_loop = asyncio.get_event_loop()
         
-    async def send(self, ws, command):
-        await ws.send(command + '\r\n')
+    async def send(self, command):
+        await self.ws.send(command + '\r\n')
         if not command.startswith('PASS'):
             print('<', command)
 
-    # async def privmsg(self, ws, command):
-    #     to_send = 'PRIVMSG' + command
-    #     ws.send()
+    async def send_msg(self, channel, text):
+        await self.send(f'PRIVMSG #{channel} :{text}')
 
-    async def run(self):
-        async with websockets.connect(uri) as ws:
-            await self.send(ws, f'PASS {self.token}')
-            await self.send(ws, f'NICK {self.nick}')
+    def run(self):
+        async def start():
+            self.ws = await websockets.connect(uri)
+            await self.send(f'PASS {self.token}')
+            await self.send(f'NICK {self.nick}')
             for channel in self.channels:
-                await self.send(ws, f'JOIN #{channel}')
+                await self.send(f'JOIN #{channel}')
+                await self.send_msg(channel, 'Ooh, Hi!')
 
             while True:
-                received_msgs = await ws.recv()
+                received_msgs = await self.ws.recv()
                 for received_msg in split(received_msgs, '\r\n'):
                     await self.handle_message(received_msg)
-    
-    # async def run(self):
-    #     asyncio.get_event_loop().run_until_complete(self.run_ws())
-    #     asyncio.get_event_loop().run_forever()
-            
         
+        self.event_loop.run_until_complete(start())
+
+
 
     async def handle_message(self, msg):
         print('>', msg)
+        if msg.startswith('PING'):
+            await self.send('PONG :tmi.twitch.tv')
 
 # asyncio.get_event_loop().run_until_complete(hello())
--- src/test.py
+++ src/test.py
@@ -3,5 +3,4 @@ import config
 import asyncio
 
 bot = client.Client(config.token, config.nick, config.channels)
-
-asyncio.get_event_loop().run_until_complete(bot.run())
+bot.run()
